997801defa1bb6ac545591dc116fc1f1e74e843f
jonnamaatta <62879239+jonnamaatta@users.noreply.github.com>
2023-01-06 17:53:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
